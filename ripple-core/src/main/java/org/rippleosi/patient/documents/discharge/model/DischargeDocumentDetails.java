26ebfc70bb2ee252b792ab1f16813b19570d9593
james-answer <CoxJ@ANS-A312.upco.co.uk>
2016-04-14 08:42:30 +0100

    Added all the required fields for the discharge detail view object.

--- ripple-core/src/main/java/org/rippleosi/patient/documents/discharge/model/DischargeDocumentDetails.java
+++ ripple-core/src/main/java/org/rippleosi/patient/documents/discharge/model/DischargeDocumentDetails.java
@@ -19,12 +19,230 @@ package org.rippleosi.patient.documents.discharge.model;
  */
 public class DischargeDocumentDetails {
     
+    private String sourceId;
     private String documentType;
     private String documentSource;
-    private String documentOrigionalSource;
-    
     private String documentDate;
-
-    private String dischargeDate;
     
+    private String date_time;
+    
+    private String author_name;
+    private String author_id;
+    private String author_idScheme;
+    
+    private String facility;
+    
+    private String patientIdentifier_mrn;
+    private String patientIdentifier_mrnType;
+    private String patientIdentifier_oth;
+    private String patientIdentifier_othType;
+    private String patientIdentifier_gms;
+    private String patientIdentifier_gmsType;
+    
+    private String dateOfAdmission;
+    
+    private String responseableProfessional_name;
+    private String responseableProfessional_id;
+    private String responseableProfessional_idType;
+    
+    private String dischargingOrganisation;
+    
+    private String dateTimeOfDischarge;
+    
+    private String dischargeDiagnosis_name;
+    private String dischargeDiagnosis_time;
+    
+    private String clinicalSynopsis;
+
+    public String getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(String sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+
+    public String getDocumentSource() {
+        return documentSource;
+    }
+
+    public void setDocumentSource(String documentSource) {
+        this.documentSource = documentSource;
+    }
+
+    public String getDocumentDate() {
+        return documentDate;
+    }
+
+    public void setDocumentDate(String documentDate) {
+        this.documentDate = documentDate;
+    }
+
+    public String getDate_time() {
+        return date_time;
+    }
+
+    public void setDate_time(String date_time) {
+        this.date_time = date_time;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public String getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(String author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getAuthor_idScheme() {
+        return author_idScheme;
+    }
+
+    public void setAuthor_idScheme(String author_idScheme) {
+        this.author_idScheme = author_idScheme;
+    }
+
+    public String getFacility() {
+        return facility;
+    }
+
+    public void setFacility(String facility) {
+        this.facility = facility;
+    }
+
+    public String getPatientIdentifier_mrn() {
+        return patientIdentifier_mrn;
+    }
+
+    public void setPatientIdentifier_mrn(String patientIdentifier_mrn) {
+        this.patientIdentifier_mrn = patientIdentifier_mrn;
+    }
+
+    public String getPatientIdentifier_mrnType() {
+        return patientIdentifier_mrnType;
+    }
+
+    public void setPatientIdentifier_mrnType(String patientIdentifier_mrnType) {
+        this.patientIdentifier_mrnType = patientIdentifier_mrnType;
+    }
+
+    public String getPatientIdentifier_oth() {
+        return patientIdentifier_oth;
+    }
+
+    public void setPatientIdentifier_oth(String patientIdentifier_oth) {
+        this.patientIdentifier_oth = patientIdentifier_oth;
+    }
+
+    public String getPatientIdentifier_othType() {
+        return patientIdentifier_othType;
+    }
+
+    public void setPatientIdentifier_othType(String patientIdentifier_othType) {
+        this.patientIdentifier_othType = patientIdentifier_othType;
+    }
+
+    public String getPatientIdentifier_gms() {
+        return patientIdentifier_gms;
+    }
+
+    public void setPatientIdentifier_gms(String patientIdentifier_gms) {
+        this.patientIdentifier_gms = patientIdentifier_gms;
+    }
+
+    public String getPatientIdentifier_gmsType() {
+        return patientIdentifier_gmsType;
+    }
+
+    public void setPatientIdentifier_gmsType(String patientIdentifier_gmsType) {
+        this.patientIdentifier_gmsType = patientIdentifier_gmsType;
+    }
+
+    public String getDateOfAdmission() {
+        return dateOfAdmission;
+    }
+
+    public void setDateOfAdmission(String dateOfAdmission) {
+        this.dateOfAdmission = dateOfAdmission;
+    }
+
+    public String getResponseableProfessional_name() {
+        return responseableProfessional_name;
+    }
+
+    public void setResponseableProfessional_name(String responseableProfessional_name) {
+        this.responseableProfessional_name = responseableProfessional_name;
+    }
+
+    public String getResponseableProfessional_id() {
+        return responseableProfessional_id;
+    }
+
+    public void setResponseableProfessional_id(String responseableProfessional_id) {
+        this.responseableProfessional_id = responseableProfessional_id;
+    }
+
+    public String getResponseableProfessional_idType() {
+        return responseableProfessional_idType;
+    }
+
+    public void setResponseableProfessional_idType(String responseableProfessional_idType) {
+        this.responseableProfessional_idType = responseableProfessional_idType;
+    }
+
+    public String getDischargingOrganisation() {
+        return dischargingOrganisation;
+    }
+
+    public void setDischargingOrganisation(String dischargingOrganisation) {
+        this.dischargingOrganisation = dischargingOrganisation;
+    }
+
+    public String getDateTimeOfDischarge() {
+        return dateTimeOfDischarge;
+    }
+
+    public void setDateTimeOfDischarge(String dateTimeOfDischarge) {
+        this.dateTimeOfDischarge = dateTimeOfDischarge;
+    }
+
+    public String getDischargeDiagnosis_name() {
+        return dischargeDiagnosis_name;
+    }
+
+    public void setDischargeDiagnosis_name(String dischargeDiagnosis_name) {
+        this.dischargeDiagnosis_name = dischargeDiagnosis_name;
+    }
+
+    public String getDischargeDiagnosis_time() {
+        return dischargeDiagnosis_time;
+    }
+
+    public void setDischargeDiagnosis_time(String dischargeDiagnosis_time) {
+        this.dischargeDiagnosis_time = dischargeDiagnosis_time;
+    }
+
+    public String getClinicalSynopsis() {
+        return clinicalSynopsis;
+    }
+
+    public void setClinicalSynopsis(String clinicalSynopsis) {
+        this.clinicalSynopsis = clinicalSynopsis;
+    }
 }
