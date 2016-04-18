9238b9da5f854337135966728098de50e3f54659
james-answer <CoxJ@ANS-A312.upco.co.uk>
2016-04-18 10:26:04 +0100

    Created the referral document detail view model

--- ripple-core/src/main/java/org/rippleosi/patient/documents/referral/model/ReferralDocumentDetails.java
+++ ripple-core/src/main/java/org/rippleosi/patient/documents/referral/model/ReferralDocumentDetails.java
@@ -15,6 +15,7 @@
  */
 package org.rippleosi.patient.documents.referral.model;
 
+import java.util.List;
 import org.rippleosi.patient.documents.common.model.AbstractDocumentSummary;
 
 /**
@@ -23,4 +24,61 @@ public class ReferralDocumentDetails extends AbstractDocumentSummary {
         
     private String documentOrigionalSource;
     private String dischargeDate;
+    
+    private String composerName;
+    private String facility;
+    private String providerId;
+    
+    private String referralDateTime;
+    private String referralType;
+    private List<String> reasonForReferral;         // Repeating element
+    private String referralComments;
+    private String priorityOfReferral;
+    private String referralReferenceNumber;
+    private String referredFrom;
+    private String referredTo;
+    
+    private String providerContact_organisationName;
+    private String providerContact_id;
+    private String providerContact_workNumber;
+    private String providerContact_emergencyNumber;
+    private String providerContact_email;
+    
+    private String referralStatus_code;
+    private String referralStatus_value;
+    private String referralStatus_mapped; // Mapped from code to origional message value
+    
+    private String clinicalNarrative;
+    private String presentIllness;
+    private String clinicalSynopsisComments;
+    private String previousHospitalAttendance;
+    
+    List<NameDateElement> pastIllensses;            // Repeating element
+    
+    List<NameDateElement> surgicalProcedures;       // Repeating element
+    
+    List<MedicationDetails> medications;              // Repeating element
+    
+    private String medication_anticoagulation_use;
+
+    List<AllergyDetails> allergies;                // Repeating element
+    
+    private String tobaccoUse;
+    private String alcholUse;
+    private String physicalImparement;
+
+    private String systolicBP;
+    private String systolicBP_units;
+    private String diastolicBP;
+    private String diastolicBP_units;
+    private String pulse;
+    private String pulse_units;
+    private String height;
+    private String height_units;
+    private String weight;
+    private String weight_units;
+    private String bodyMass;
+    private String bodyMass_units;
+    
+    private String otherExaminationFindings;
 }
