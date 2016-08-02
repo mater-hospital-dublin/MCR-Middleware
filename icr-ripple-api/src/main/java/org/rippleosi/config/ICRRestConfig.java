cd60574686acab44a47170fa50864fd0cd3d93f9
Josh.Hilland <josh.hilland@answerdigital.com>
2016-08-02 13:25:18 +0100

    Reverted inclusion of Camel routes for writes

--- icr-ripple-api/src/main/java/org/rippleosi/config/ICRRestConfig.java
+++ icr-ripple-api/src/main/java/org/rippleosi/config/ICRRestConfig.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package org.rippleosi.config.api;
+package org.rippleosi.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
